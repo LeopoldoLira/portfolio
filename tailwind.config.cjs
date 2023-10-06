506b4df854492a50bc5a95c02242ba67b438ec23
Leopoldo Lira <lira.leopoldo97@gmail.com>
2023-10-05 22:48:17 -0600

    feat(core)!:Redesign whole Landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+
 		extend: {
 			animation:{
 				'spin-slow':'spin 10s linear infinite'
+			},
+			gridTemplateRows:{
+				'layout':'20rem 20rem 20rem 20rem',
 			}
 		},
 	},
