e8752e394b046ff64d2af8af0c9a83b587f66e10
Leopoldo Lira <lira.leopoldo97@gmail.com>
2023-10-19 22:28:59 -0600

    Release(Portfolio)!: Version 1.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,16 @@ module.exports = {
 			},
 			gridTemplateRows:{
 				'layout':'repeat(6,17.5rem)',
-				'tabletlayout':'repeat(6,20rem)',
-				'mobilelargelayout':'repeat(11, 11.5rem)'
+				'tabletlayout':'repeat(11,20rem)',
+				'mobilelargelayout':'repeat(11, 11.5rem)',
+				'smallLaptop':'repeat(6,15rem)',
+				'4k':'repeat(6,30rem)'
 			}
 		},
 
 		screens:{
+			'4k':{'min':'2560px'},
+			'smallLaptop':{'max':'1024px'},
 			'tablet':{'max':'768px'},
 			'mobile-l':{'max':'425px'}
 		}
