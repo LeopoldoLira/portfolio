9fab2ddc5b0edf049d39fa18cc3c0142914d098c
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-03-27 22:27:30 -0600

    fix(homepage)!:fixed the whole grid layout for all screen. modified breakpoints in tailwind config file.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,24 +3,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 
-		extend: {
-			animation:{
-				'spin-slow':'spin 10s linear infinite'
-			},
-			gridTemplateRows:{
-				'layout':'repeat(6,17.5rem)',
-				'tabletlayout':'repeat(11,20rem)',
-				'mobilelargelayout':'repeat(11, 9.5rem)',
-				'smallLaptop':'repeat(6,15rem)',
-				'4k':'repeat(6,23rem)'
-			}
-		},
-
 		screens:{
-			'4k':{'max':'1920px'},
-			'smallLaptop':{'max':'1024px'},
-			'tablet':{'max':'768px'},
-			'mobile-l':{'max':'425px'}
+
+			'mobile':{'min':'1px', 'max':'425px'},
+			'tablet':{'min':'426px', 'max':'768px'},
+			'sm-laptop':{'min':'769px','max':'1024px'},
+			'laptop':{'min':'1025px', 'max':'1440px'},
+			'monitor':{'min':'1441px'},
 		}
 	},
 	plugins: [],
